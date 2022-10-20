e50af812bba2f8b8d79af0a5d69fd72b250182bc
evanmacbride <ejmacbride@gmail.com>
2022-10-20 16:43:44 -0500

    clean up comments and print-outs, create simul() wrapper function

--- guess.py
+++ guess.py
@@ -148,7 +148,6 @@ class Guess:
 		i = 0
 		while redundant:
 			breaker = breaker_word_pts[i][0]
-			print("Considering:",breaker.upper())
 			i += 1
 			if breaker in self.guess_history:
 				continue
@@ -259,96 +258,101 @@ class Game:
 		self.score_history.append(score)
 		return score
 
-# Run simulation to calculate win rate
-max_simuls = 1000
-games_won = 0
-breaker_wins = []
-breaker_losses = []
-loss_solns = []
-for i in range(max_simuls):
-	game = Game("data/enable1.txt")
-	game_words = game.get_word_list()
-	true_soln = game.get_solution()
-	#true_soln = 'kinks'
-	#game.solution = true_soln
-	guess = Guess(game_words)
-	print(true_soln.upper())
-	used_breaker = False
-	breaker_last_turn = False
-	breaker_info = None
-	strict = False
-	all_similar = False
-	# Game loop
-	while not game.won and game.current_turn < Game.TURNS:
-		turns_remaining = Game.TURNS - game.current_turn - 1
-		# Check how much of the solution we have
-		matched_ltrs = 0
-		for l in guess.soln_ltr_matches:
-			if l is not None and l == 2:
-				matched_ltrs += 1
-		if game.current_turn >= 2:
-			all_similar =  guess.lookahead_similarity()
-		if game.current_turn == 0:
-			cur_guess = guess.add_manual_guess('crane')
-			breaker_last_turn = False
-		elif guess.seen_complete_soln():
-			print("***STRICT MODE*** solution seen")
-			strict = True
-			cur_guess = guess.get_auto_guess(strict)
-			breaker_last_turn = False
-		elif len(guess.strict_word_list) <= turns_remaining + 1:
-			print("***STRICT MODE*** few choices remain")
-			strict = True
-			cur_guess = guess.get_auto_guess(strict)
-			breaker_last_turn = False
-		elif game.current_turn == Game.TURNS - 1:
-			print("***STRICT MODE*** final turn")
-			strict = True
-			cur_guess = guess.get_auto_guess(strict)
-			breaker_last_turn = False
-		elif len(guess.soln_ltrs) >= Game.WORD_LEN-1 or matched_ltrs >= Game.WORD_LEN-1 or all_similar:
-			print("***BREAKER MODE*** only similar words remain")
-			strict = True
-			# Only log the word_list length for the first time find_breaker() is called.
-			if not used_breaker:
-				breaker_info = len(guess.strict_word_list)
-			cur_guess = guess.find_breaker(hard=True)
-			used_breaker = True
-			breaker_last_turn = True
-		elif game.current_turn >= Game.TURNS - 4 and len(guess.strict_word_list) > 4 and not breaker_last_turn:
-			print("***BREAKER MODE*** too many words remain")
-			strict = True
-			if not used_breaker:
-				breaker_info = len(guess.strict_word_list)
-			cur_guess = guess.find_breaker(hard=True)
-			used_breaker = True
-			breaker_last_turn = True
+def simul(num_simuls=10, verbose=True, manual_soln=None):
+	# Run simulation to calculate win rate
+	games_won = 0
+	loss_solns = []
+	for i in range(num_simuls):
+		# Initialize game and guess engine
+		game = Game("data/enable1.txt")
+		game_words = game.get_word_list()
+		if not manual_soln:
+			true_soln = game.get_solution()
 		else:
-			if strict:
-				print("***STRICT MODE***")
-			cur_guess = guess.get_auto_guess(strict)
-			breaker_last_turn = False
-		score = game.score_guess(cur_guess)
-		print(turns_remaining, cur_guess.upper(), score, guess.strict_word_list[0:10],
-					"...", len(guess.strict_word_list))
-		if cur_guess == true_soln:
-			print("YOU WIN")
-			game.won = True
-			games_won += 1
-			if used_breaker:
-				breaker_wins.append(breaker_info)
-			break
-		else:
-			game.current_turn += 1
-		guess.update(score)
-	if not game.won:
-		print("YOU LOSE")
-		print("REMAINING WORDS: ", guess.strict_word_list)
-		loss_solns.append(true_soln)
-		if used_breaker:
-			breaker_losses.append(breaker_info)
-	print()
-print(games_won, "/", max_simuls, "... WIN RATE:", games_won / max_simuls)
-print("LOST ON:")
-for s in loss_solns:
-	print(s)
+			true_soln = manual_soln
+			game.solution = true_soln
+		guess = Guess(game_words)
+		# Gameplay booleans
+		used_breaker = False
+		breaker_last_turn = False
+		strict = False
+		all_similar = False
+		# Game loop
+		while not game.won and game.current_turn < Game.TURNS:
+			turns_remaining = Game.TURNS - game.current_turn - 1
+			# Check how much of the solution we have
+			matched_ltrs = 0
+			for l in guess.soln_ltr_matches:
+				if l is not None and l == 2:
+					matched_ltrs += 1
+			if game.current_turn >= 2:
+				all_similar =  guess.lookahead_similarity()
+			if game.current_turn == 0:
+				cur_guess = guess.add_manual_guess('crane')
+				breaker_last_turn = False
+			elif guess.seen_complete_soln():
+				if verbose:
+					print("***STRICT MODE*** solution seen")
+				strict = True
+				cur_guess = guess.get_auto_guess(strict)
+				breaker_last_turn = False
+			elif len(guess.strict_word_list) <= turns_remaining + 1:
+				if verbose:
+					print("***STRICT MODE*** few choices remain")
+				strict = True
+				cur_guess = guess.get_auto_guess(strict)
+				breaker_last_turn = False
+			elif game.current_turn == Game.TURNS - 1:
+				if verbose:
+					print("***STRICT MODE*** final turn")
+				strict = True
+				cur_guess = guess.get_auto_guess(strict)
+				breaker_last_turn = False
+			elif len(guess.soln_ltrs) >= Game.WORD_LEN-1 or matched_ltrs >= Game.WORD_LEN-1 or all_similar:
+				if verbose:
+					print("***BREAKER MODE*** only similar words remain")
+				strict = True
+				cur_guess = guess.find_breaker(hard=True)
+				breaker_last_turn = True
+			elif game.current_turn >= Game.TURNS - 4 and len(guess.strict_word_list) > 4 and not breaker_last_turn:
+				if verbose:
+					print("***BREAKER MODE*** too many words remain")
+				strict = True
+				cur_guess = guess.find_breaker(hard=True)
+				breaker_last_turn = True
+			else:
+				if strict and verbose:
+					print("***STRICT MODE***")
+				cur_guess = guess.get_auto_guess(strict)
+				breaker_last_turn = False
+			score = game.score_guess(cur_guess)
+			if verbose:
+				print(turns_remaining, cur_guess.upper(), score, guess.strict_word_list[0:10],
+						"...", len(guess.strict_word_list))
+			else:
+				print(turns_remaining, cur_guess.upper(), score)
+			if cur_guess == true_soln:
+				print("YOU WIN")
+				game.won = True
+				games_won += 1
+				break
+			else:
+				game.current_turn += 1
+			guess.update(score)
+		if not game.won:
+			print("YOU LOSE")
+			print("SOLN WAS:", true_soln.upper())
+			print("Remaining words:") 
+			for word in guess.strict_word_list:
+				print(word)
+			loss_solns.append(true_soln)
+		print()
+	print(games_won, "/", num_simuls, "... WIN RATE:", games_won / num_simuls)
+	if num_simuls != games_won:
+		print("LOST ON:")
+		for s in loss_solns:
+			print(s)
+	return
+
+if __name__ == '__main__':
+	sys.exit(simul())
